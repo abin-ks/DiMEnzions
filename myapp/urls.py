803808d3dd82c958d7a6a28237cae95c54ff9c63
Abin KS <abinks056@gmail.com>
2022-03-22 16:55:22 +0530

    login and register max.completed

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import re_path
 from .import views
 
 urlpatterns = [
-    re_path(r'^$', views.admin_dashboard, name='admin_dashboard'),
+    re_path(r'^$', views.admin_log, name='admin_log'),
+    re_path(r'^admin_login$', views.admin_login, name='admin_login'),
+    re_path(r'^admin_dashboard$', views.admin_dashboard, name='admin_dashboard'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render, redirect
+from .models import *
 
 
 
 # Create your views here.
+
+
+def admin_log(request):
+    return render(request, 'admin_log.html')
+
+def admin_login(request):
+    # try:
+        if request.method == 'POST':
+            if Admin_register.objects.filter(username = request.POST['username'], password = request.POST['password']).exists():
+                member = Admin_register.objects.get(username = request.POST['username'], password = request.POST['password'])
+                request.session['admid'] = member.reg_id
+                
+                return redirect('admin_dashboard')
+            else:
+                return redirect('admin_log')
+        else:
+            return redirect('admin_log')
+    # except:
+    #     return redirect('admin_log')
+    
 def admin_dashboard(request):
-    return render(request, 'admin_dashboard.html')
+    admid = request.session['admid']
+    adm = Admin_register.objects.filter(reg_id=admid)
+    return render(request, 'admin_dashboard.html', {'adm':adm})
